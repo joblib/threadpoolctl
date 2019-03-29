4325dac8ed728715162cb92aa32996923dbebf24
Olivier Grisel <olivier.grisel@ensta.org>
2019-03-29 16:45:13 +0100

    typo

--- threadpoolctl.py
+++ threadpoolctl.py
@@ -1,7 +1,7 @@
 """threadpoolctl
 
 This module provides utilities to introspect native libraries that relies on
-thread pools (notably BLAS and OpenMP implementation) and dynamically set the
+thread pools (notably BLAS and OpenMP implementations) and dynamically set the
 maximal number of threads they can use.
 """
 # License: BSD 3-Clause

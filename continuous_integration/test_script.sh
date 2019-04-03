7bbe589abb3a26003615fe22e03ca90ed134a9c0
Olivier Grisel <olivier.grisel@ensta.org>
2019-04-03 11:22:57 +0200

    Display debug info on CI

--- continuous_integration/test_script.sh
+++ continuous_integration/test_script.sh
@@ -8,6 +8,7 @@ elif [[ "$PACKAGER" == "ubuntu" ]]; then
     source $VIRTUALENV/bin/activate
 fi
 
+python -c "import numpy; from tests._openmp_test_helper_outer import *; from threadpoolctl import threadpool_info; from pprint import pprint; pprint(threadpool_info())"
 set -x
 pytest -vlx --junitxml=$JUNITXML --cov=threadpoolctl
 set +x

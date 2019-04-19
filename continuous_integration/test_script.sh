1c24ab3b626d1b713f60450522a194a0f4046803
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2019-04-19 17:17:22 +0200

    tst

--- continuous_integration/test_script.sh
+++ continuous_integration/test_script.sh
@@ -21,6 +21,7 @@ python -c "from tests.test_threadpoolctl import test_openmp_limit_num_threads, t
 
 python -c "from tests.test_threadpoolctl import test_openmp_limit_num_threads, test_nested_prange_blas; test_openmp_limit_num_threads(4); test_nested_prange_blas(1)"
 # python -c "from tests.test_threadpoolctl import test_openmp_limit_num_threads, test_nested_prange_blas; test_openmp_limit_num_threads(4); test_nested_prange_blas(2)"
+python -c "from tests.test_threadpoolctl import test_openmp_limit_num_threads, test_nested_prange_blas; test_openmp_limit_num_threads(4); test_nested_prange_blas(3)"
 python -c "from tests.test_threadpoolctl import test_openmp_limit_num_threads, test_nested_prange_blas; test_openmp_limit_num_threads(4); test_nested_prange_blas(4)"
 python -c "from tests.test_threadpoolctl import test_openmp_limit_num_threads, test_nested_prange_blas; test_openmp_limit_num_threads(4); test_nested_prange_blas(None)"
 

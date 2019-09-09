ae15620f7b7a7d2644941ed5dcd20f793fcf5c34
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2019-09-09 18:12:24 +0200

    indent

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -5,12 +5,12 @@ set -e
 UNAMESTR=`uname`
 
 if [[ "$CC_OUTER_LOOP" == "clang-8" || "$CC_INNER_LOOP" == "clang-8" ]]; then
-# Assume Ubuntu: install a recent version of clang and libomp
-echo "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-8 main" | sudo tee -a /etc/apt/sources.list.d/llvm.list
-echo "deb-src http://apt.llvm.org/xenial/ llvm-toolchain-xenial-8 main" | sudo tee -a /etc/apt/sources.list.d/llvm.list
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-sudo apt update
-sudo apt install clang-8 libomp-8-dev
+    # Assume Ubuntu: install a recent version of clang and libomp
+    echo "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-8 main" | sudo tee -a /etc/apt/sources.list.d/llvm.list
+    echo "deb-src http://apt.llvm.org/xenial/ llvm-toolchain-xenial-8 main" | sudo tee -a /etc/apt/sources.list.d/llvm.list
+    wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+    sudo apt update
+    sudo apt install clang-8 libomp-8-dev
 fi
 
 make_conda() {

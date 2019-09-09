c855827a52e33e8e7c03dc00db1d13611f70a147
jeremiedbb <34657725+jeremiedbb@users.noreply.github.com>
2019-09-09 18:14:20 +0200

    CI: Try to use conda-forge compilers on macOS (#29)

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -4,20 +4,7 @@ set -e
 
 UNAMESTR=`uname`
 
-if [[ "$UNAMESTR" == "Darwin" ]]; then
-    # Install a compiler with a working openmp
-    HOMEBREW_NO_AUTO_UPDATE=1 brew install libomp
-
-    # enable OpenMP support for Apple-clang
-    export CC=/usr/bin/clang
-    export CXX=/usr/bin/clang++
-    export CPPFLAGS="$CPPFLAGS -Xpreprocessor -fopenmp"
-    export CFLAGS="$CFLAGS -I/usr/local/opt/libomp/include"
-    export CXXFLAGS="$CXXFLAGS -I/usr/local/opt/libomp/include"
-    export LDFLAGS="$LDFLAGS -L/usr/local/opt/libomp/lib -lomp"
-    export DYLD_LIBRARY_PATH=/usr/local/opt/libomp/lib
-
-elif [[ "$CC_OUTER_LOOP" == "clang-8" || "$CC_INNER_LOOP" == "clang-8" ]]; then
+if [[ "$CC_OUTER_LOOP" == "clang-8" || "$CC_INNER_LOOP" == "clang-8" ]]; then
     # Assume Ubuntu: install a recent version of clang and libomp
     echo "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-8 main" | sudo tee -a /etc/apt/sources.list.d/llvm.list
     echo "deb-src http://apt.llvm.org/xenial/ llvm-toolchain-xenial-8 main" | sudo tee -a /etc/apt/sources.list.d/llvm.list
@@ -28,6 +15,24 @@ fi
 
 make_conda() {
     TO_INSTALL="$@"
+    if [[ "$UNAMESTR" == "Darwin" ]]; then
+        if [[ "$INSTALL_LIBOMP" == "conda-forge" ]]; then
+            # Install an OpenMP-enabled clang/llvm from conda-forge
+            TO_INSTALL="$TO_INSTALL conda-forge::compilers conda-forge::llvm-openmp"
+            export LDFLAGS="$LDFLAGS -Wl,-rpath,$CONDA/envs/$VIRTUALENV/lib -L$CONDA/envs/$VIRTUALENV/lib"
+            export CFLAGS="$CFLAGS -I$CONDA/envs/$VIRTUALENV/include"
+
+        elif [[ "$INSTALL_LIBOMP" == "homebrew" ]]; then
+            # Install a compiler with a working openmp
+            HOMEBREW_NO_AUTO_UPDATE=1 brew install libomp
+
+            # enable OpenMP support for Apple-clang
+            export CC=/usr/bin/clang
+            export CPPFLAGS="$CPPFLAGS -Xpreprocessor -fopenmp"
+            export CFLAGS="$CFLAGS -I/usr/local/opt/libomp/include"
+            export LDFLAGS="$LDFLAGS -Wl,-rpath,/usr/local/opt/libomp/lib -L/usr/local/opt/libomp/lib -lomp"
+        fi
+    fi
     conda create -n $VIRTUALENV -q --yes $TO_INSTALL
     source activate $VIRTUALENV
 }

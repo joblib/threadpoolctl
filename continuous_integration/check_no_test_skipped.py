8ead8f4b0276964b3086d5e4a74acbb475b2f6b5
Jérémie du Boisberranger <34657725+jeremiedbb@users.noreply.github.com>
2019-11-22 18:02:08 +0100

    [MRG] MNT Fix meta-test distinguish skipped from other records (#59)

--- continuous_integration/check_no_test_skipped.py
+++ continuous_integration/check_no_test_skipped.py
@@ -11,26 +11,31 @@ base_dir = sys.argv[1]
 
 # dict {test: result} where result is False if the test was skipped in every
 # job and True otherwise.
-aggregated_results = {}
+always_skipped = {}
 
 for name in os.listdir(base_dir):
     # all test result files are in /base_dir/jobs.*/ dirs
     if name.startswith("stage1."):
+        print("> processing test result from job", name.replace("stage1", ""))
+        print("  > tests skipped:")
         result_file = os.path.join(base_dir, name, "test-data.xml")
         root = ET.parse(result_file).getroot()
 
         # All tests are identified by the xml tag testcase.
-        for test in root.iter('testcase'):
-            test_name = test.attrib['name']
-            if test_name not in aggregated_results:
-                # len(test) is > 0 if the test is skipped.
-                aggregated_results[test_name] = not bool(len(test))
+        for test in root.iter("testcase"):
+            test_name = test.attrib["name"]
+            skipped = any(child.tag == "skipped" for child in test)
+            if skipped:
+                print("    -", test_name)
+            if test_name in always_skipped:
+                always_skipped[test_name] &= skipped
             else:
-                aggregated_results[test_name] |= not bool(len(test))
+                always_skipped[test_name] = skipped
 
+print("\n------------------------------------------------------------------\n")
 fail = False
-for test, result in aggregated_results.items():
-    if not result:
+for test, skipped in always_skipped.items():
+    if skipped:
         fail = True
         print(test, "was skipped in every job")
 

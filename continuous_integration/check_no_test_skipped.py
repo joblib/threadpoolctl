79cba8b48e0490ed554343f32794faaa6e245fca
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2019-11-22 15:36:52 +0100

    adress comments

--- continuous_integration/check_no_test_skipped.py
+++ continuous_integration/check_no_test_skipped.py
@@ -11,34 +11,34 @@ base_dir = sys.argv[1]
 
 # dict {test: result} where result is False if the test was skipped in every
 # job and True otherwise.
-aggregated_results = {}
+always_skipped = {}
 
 for name in os.listdir(base_dir):
     # all test result files are in /base_dir/jobs.*/ dirs
     if name.startswith("stage1."):
         print("> processing test result from job", name.replace("stage1", ""))
+        print("  > tests skipped:")
         result_file = os.path.join(base_dir, name, "test-data.xml")
         root = ET.parse(result_file).getroot()
 
         # All tests are identified by the xml tag testcase.
         for test in root.iter("testcase"):
             test_name = test.attrib["name"]
-            if test_name not in aggregated_results:
-                aggregated_results[test_name] = False
-            print("  -", test_name)
+            if test_name not in always_skipped:
+                always_skipped[test_name] = False
 
             for child in test:
-                print("    -", child.tag)
                 if child.tag == "skipped":
-                    aggregated_results[test_name] |= False
+                    print("  -", test_name)
+                    always_skipped[test_name] |= True
                     break
             else:
-                aggregated_results[test_name] |= True
+                always_skipped[test_name] = False
 
 print("\n------------------------------------------------------------------\n")
 fail = False
-for test, result in aggregated_results.items():
-    if not result:
+for test, skipped in always_skipped.items():
+    if skipped:
         fail = True
         print(test, "was skipped in every job")
 

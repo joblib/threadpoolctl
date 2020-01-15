c4eac7b47306be458d5f08302cc0019c135677b2
Olivier Grisel <olivier.grisel@ensta.org>
2020-01-15 17:57:59 +0100

    Use the CLI instead of continuous_integration/display_versions.py

--- continuous_integration/test_script.cmd
+++ continuous_integration/test_script.cmd
@@ -1,5 +1,7 @@
 call activate %VIRTUALENV%
 
-python continuous_integration/display_versions.py
+# Use the CLI to display the effective runtime environment prior to
+# launching the tests:
+python -m threadpoolctl -i numpy scipy.linalg tests._openmp_test_helper
 
 pytest -vlrxXs --junitxml=%JUNITXML% --cov=threadpoolctl
--- continuous_integration/test_script.sh
+++ continuous_integration/test_script.sh
@@ -12,6 +12,9 @@ elif [[ "$PACKAGER" == "ubuntu" ]]; then
 fi
 
 set -x
-PYTHONPATH="." python continuous_integration/display_versions.py
+
+# Use the CLI to display the effective runtime environment prior to
+# launching the tests:
+python -m threadpoolctl -i numpy scipy.linalg tests._openmp_test_helper
 
 pytest -vlrxXs -W error -k "$TESTS" --junitxml=$JUNITXML --cov=threadpoolctl

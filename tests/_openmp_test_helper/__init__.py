9d83dfc23500eb089e6ddd3acab38b183085f798
Olivier Grisel <olivier.grisel@ensta.org>
2020-01-16 15:05:02 +0100

    Use the CLI instead of continuous_integration/display_versions.py (#66)

--- tests/_openmp_test_helper/__init__.py
+++ tests/_openmp_test_helper/__init__.py
@@ -2,7 +2,12 @@ from .openmp_helpers_inner import check_openmp_num_threads
 from .openmp_helpers_inner import get_compiler as get_inner_compiler
 from .openmp_helpers_outer import check_nested_openmp_loops
 from .openmp_helpers_outer import get_compiler as get_outer_compiler
-from .nested_prange_blas import check_nested_prange_blas
+
+try:
+    from .nested_prange_blas import check_nested_prange_blas
+except ImportError:
+    # Can happen if numpy and scipy are missing.
+    check_nested_prange_blas = None
 
 
 __all__ = ["check_openmp_num_threads",

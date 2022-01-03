0b76272514c857ed72da23321a2068ba84a57874
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2022-01-03 17:06:34 +0100

    tst import specific extension

--- continuous_integration/test_script.cmd
+++ continuous_integration/test_script.cmd
@@ -2,6 +2,6 @@ call activate %VIRTUALENV%
 
 # Use the CLI to display the effective runtime environment prior to
 # launching the tests:
-python -m threadpoolctl -i numpy scipy.linalg tests._openmp_test_helper
+python -m threadpoolctl -i numpy scipy.linalg tests._openmp_test_helper.openmp_helpers_inner
 
 pytest -vlrxXs --junitxml=%JUNITXML% --cov=threadpoolctl
--- continuous_integration/test_script.sh
+++ continuous_integration/test_script.sh
@@ -15,6 +15,6 @@ set -x
 
 # Use the CLI to display the effective runtime environment prior to
 # launching the tests:
-python -m threadpoolctl -i numpy scipy.linalg tests._openmp_test_helper
+python -m threadpoolctl -i numpy scipy.linalg tests._openmp_test_helper.openmp_helpers_inner
 
 pytest -vlrxXs -W error -k "$TESTS" --junitxml=$JUNITXML --cov=threadpoolctl

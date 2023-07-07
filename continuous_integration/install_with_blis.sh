79c821c8c313f0a2c9bca5b62e081a7ff20d7712
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2023-07-07 14:02:22 +0200

    ???

--- continuous_integration/install_with_blis.sh
+++ continuous_integration/install_with_blis.sh
@@ -42,6 +42,7 @@ library_dirs = $ABS_PATH/BLIS_install/lib
 include_dirs = $ABS_PATH/BLIS_install/include/blis
 runtime_library_dirs = $ABS_PATH/BLIS_install/lib" > site.cfg
 python setup.py build_ext -i
+python setup.py build_ext -i  # WTF
 pip install -e .
 popd
 

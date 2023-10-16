0cc1e84cd0a6b07d630f2a90c875981c09c050fb
Olivier Grisel <olivier.grisel@ensta.org>
2023-10-16 18:30:46 +0200

    Display the packages installed in the test environment on Windows

--- continuous_integration/test_script.cmd
+++ continuous_integration/test_script.cmd
@@ -1,5 +1,8 @@
 call activate %VIRTUALENV%
 
+# Display version information
+python -m pip list
+
 # Use the CLI to display the effective runtime environment prior to
 # launching the tests:
 python -m threadpoolctl -i numpy scipy.linalg tests._openmp_test_helper.openmp_helpers_inner

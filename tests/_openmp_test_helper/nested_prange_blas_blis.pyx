1644199c3e5bf56b0c2b7390d9cce948a12bdeae
Jérémie du Boisberranger <34657725+jeremiedbb@users.noreply.github.com>
2023-10-17 18:41:37 +0200

    TST Remove cython conditional compilation (#152)
    Co-authored-by: Olivier Grisel <olivier.grisel@ensta.org>

--- tests/_openmp_test_helper/nested_prange_blas_blis.pyx
+++ tests/_openmp_test_helper/nested_prange_blas_blis.pyx
@@ -2,7 +2,20 @@ cimport openmp
 from cython.parallel import parallel, prange
 
 import numpy as np
-from scipy.linalg.cython_blas cimport dgemm
+
+cdef extern from 'cblas.h' nogil:
+    ctypedef enum CBLAS_ORDER:
+        CblasRowMajor=101
+        CblasColMajor=102
+    ctypedef enum CBLAS_TRANSPOSE:
+        CblasNoTrans=111
+        CblasTrans=112
+        CblasConjTrans=113
+    void dgemm 'cblas_dgemm' (
+        CBLAS_ORDER Order, CBLAS_TRANSPOSE TransA,
+        CBLAS_TRANSPOSE TransB, int M, int N,
+        int K, double alpha, double *A, int lda,
+        double *B, int ldb, double beta, double *C, int ldc)
 
 from threadpoolctl import ThreadpoolController
 
@@ -18,8 +31,6 @@ def check_nested_prange_blas(double[:, ::1] A, double[:, ::1] B, int nthreads):
         int n_chunks = 100
         int chunk_size = A.shape[0] // n_chunks
 
-        char* trans = 't'
-        char* no_trans = 'n'
         double alpha = 1.0
         double beta = 0.0
 
@@ -37,8 +48,8 @@ def check_nested_prange_blas(double[:, ::1] A, double[:, ::1] B, int nthreads):
             prange_num_threads_ptr[0] = openmp.omp_get_num_threads()
 
         for i in prange(n_chunks):
-            dgemm(trans, no_trans, &n, &chunk_size, &k,
-                &alpha, &B[0, 0], &k, &A[i * chunk_size, 0], &k,
-                &beta, &C[i * chunk_size, 0], &n)
+            dgemm(CblasRowMajor, CblasNoTrans, CblasTrans,
+            chunk_size, n, k, alpha, &A[i * chunk_size, 0], k,
+            &B[0, 0], k, beta, &C[i * chunk_size, 0], n)
 
     return np.asarray(C), prange_num_threads, inner_info[0]

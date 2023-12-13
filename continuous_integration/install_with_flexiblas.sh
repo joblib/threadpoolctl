2c132a3b344599d3988add89b0c82e6d898940b6
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2023-12-13 16:28:58 +0100

    add flexiblas job

---
+++ continuous_integration/install_with_flexiblas.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+
+pushd ..
+ABS_PATH=$(pwd)
+popd
+
+# create conda env
+conda update -n base conda conda-libmamba-solver -q --yes
+conda config --set solver libmamba
+conda create -n $VIRTUALENV -q --yes -c conda-forge python=$PYTHON_VERSION pip cython
+source activate $VIRTUALENV
+
+pushd ..
+
+# install different blas libraries
+conda install -c conda-forge blis mkl openblas
+
+# install flexiblas
+mkdir flexiblas_install
+git clone https://github.com/mpimd-csc/flexiblas.git
+pushd flexiblas
+
+mkdir build
+pushd build
+cmake ../ -DCMAKE_INSTALL_PREFIX=$ABS_PATH"/flexiblas_install" \
+    -DEXTRA="OpenBLAS;BLIS;MKL" \
+    -DOpenBLAS_LIBRARY=$CONDA_PREFIX"/envs/tmp/lib/libopenblas.so" \
+    -DBLIS_LIBRARY=$CONDA_PREFIX"/envs/tmp/lib/libblis.so" \
+    -DMKL_LIBRARY=$CONDA_PREFIX"/envs/tmp/lib/libmkl_rt.so"
+make
+make install
+popd
+
+popd
+
+python -m pip install -q -r dev-requirements.txt
+CFLAGS=-I$ABS_PATH/flexiblas_install/include/ \
+    LDFLAGS=-L$ABS_PATH/flexiblas_install/lib \
+    bash ./continuous_integration/build_test_ext.sh
+
+python --version
+python -c "import numpy; print(f'numpy {numpy.__version__}')" || echo "no numpy"
+python -c "import scipy; print(f'scipy {scipy.__version__}')" || echo "no scipy"
+
+python -m flit install --symlink

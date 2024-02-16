dc72b82a98d914fbd6f6db3eaeb27e1b68f65be5
Loïc Estève <loic.esteve@ymail.com>
2024-02-16 07:16:17 +0100

    Tweak comment

--- threadpoolctl.py
+++ threadpoolctl.py
@@ -1105,7 +1105,12 @@ class ThreadpoolController:
     def _find_libraries_pyodide(self):
         """Pyodide specific implementation for finding loaded libraries.
 
-        Adapted from suggestion in https://github.com/joblib/threadpoolctl/pull/169#issuecomment-1946696449
+        Adapted from suggestion in https://github.com/joblib/threadpoolctl/pull/169#issuecomment-1946696449.
+
+        One day, we may have a simpler solution. libc dl_iterate_phdr needs to
+        be implemented in Emscripten and exposed in Pyodide, see
+        https://github.com/emscripten-core/emscripten/issues/21354 for more
+        details.
         """
         try:
             from pyodide_js._module import LDSO

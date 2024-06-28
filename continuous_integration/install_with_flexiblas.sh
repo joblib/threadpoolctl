f40f5a7ecf9df35bf7bd77ead3838888fd901af5
Jérémie du Boisberranger <jeremie@probabl.ai>
2024-06-28 16:54:17 +0200

    pin older llvm openmp version

--- continuous_integration/install_with_flexiblas.sh
+++ continuous_integration/install_with_flexiblas.sh
@@ -10,7 +10,8 @@ popd
 conda update -n base conda conda-libmamba-solver -q --yes
 conda config --set solver libmamba
 conda create -n $VIRTUALENV -q --yes -c conda-forge python=$PYTHON_VERSION \
-    pip cython openblas $PLATFORM_SPECIFIC_PACKAGES meson-python pkg-config compilers
+    pip cython openblas $PLATFORM_SPECIFIC_PACKAGES meson-python pkg-config compilers \
+    llvm-openmp=18.1.5
 source activate $VIRTUALENV
 
 pushd ..
@@ -19,7 +20,6 @@ pushd ..
 mkdir flexiblas_install
 git clone https://github.com/mpimd-csc/flexiblas.git
 pushd flexiblas
-git checkout v3.4.4
 
 mkdir build
 pushd build

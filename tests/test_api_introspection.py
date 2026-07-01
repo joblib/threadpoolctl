95d77a050c21aa301bd3b6f3626464eb40d0bb4e
Itamar Turner-Trauring <itamar@itamarst.org>
2026-07-01 11:10:48 -0400

    Try to reintroduce a test that checks real libraries.

--- tests/test_api_introspection.py
+++ tests/test_api_introspection.py
@@ -2,11 +2,19 @@
 Tests for get/set number of threads API introspection.
 """
 
+import sys
 from threading import local as threadlocal
 
 import pytest
 
-from threadpoolctl import _ThreadLimitScope, _determine_thread_limit_scope
+from threadpoolctl import (
+    _ThreadLimitScope,
+    _determine_thread_limit_scope,
+    ThreadpoolController,
+)
+
+# Make sure we have some BLAS libraries loaded:
+from . import utils as _
 
 
 class FakeThreadLocalAPI(threadlocal):
@@ -52,3 +60,38 @@ def test_determine_thread_limit_scope_processwide(default: int) -> None:
     """
     api = FakeProcesswideAPI(default)
     assert _determine_thread_limit_scope(api.get, api.set) == _ThreadLimitScope.PROCESS
+
+
+@pytest.mark.skipif(
+    sys.platform != "linux", reason="We only hardcoded Linux-specific behavior"
+)
+@pytest.mark.parametrize(
+    ["select_filter", "expected_thread_limit_scope"],
+    [
+        (
+            {"internal_api": "openblas", "threading_layer": "pthreads"},
+            _ThreadLimitScope.PROCESS,
+        ),
+        (
+            {"user_api": "openmp"},
+            _ThreadLimitScope.CURRENT_THREAD,
+        ),
+    ],
+)
+def test_api_scope(
+    select_filter: dict[str, str], expected_thread_limit_scope: str
+) -> None:
+    """
+    Check ``_determine_thread_limit_scope()`` against libraries with known
+    properties, to make sure it detects them correctly.  The test is intended
+    to be of the function's behavior, not of the libraries.
+    """
+    controller = ThreadpoolController().select(**select_filter)
+    if not controller.lib_controllers:
+        pytest.skip(f"{select_filter} controller not found")
+
+    for lib in controller.lib_controllers:
+        assert (
+            _determine_thread_limit_scope(lib.get_num_threads, lib.set_num_threads)
+            == expected_thread_limit_scope
+        )

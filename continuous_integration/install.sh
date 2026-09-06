eaaa9a4611b64de1bf4c89736845a154874b0114
Loïc Estève <loic.esteve@ymail.com>
2026-09-06 06:44:58 +0200

    try old source trick

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -54,6 +54,7 @@ make_conda() {
     conda config --set solver libmamba
 
     conda create -n testenv -q --yes python=$PYTHON_VERSION $TO_INSTALL
+    source "$(conda info --base)/etc/profile.d/conda.sh"
     conda activate testenv
     echo "$PATH"
     type -a clang

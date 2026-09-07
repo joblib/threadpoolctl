261389fcbde97f46e88f80df6884be3d3d64781c
Loïc Estève <loic.esteve@ymail.com>
2026-09-07 11:12:13 +0200

    CI Fix compilation issue with OpenMP inside conda env (#237)

--- continuous_integration/run_tests.sh
+++ continuous_integration/run_tests.sh
@@ -3,10 +3,12 @@
 set -xe
 
 if [[ "$PACKAGER" == conda* ]] || [[ -z "$PACKAGER" ]]; then
+    source "$CONDA/etc/profile.d/conda.sh"
     conda activate testenv
     conda list
 elif [[ "$PACKAGER" == pip* ]]; then
     # we actually use conda to install the base environment:
+    source "$CONDA/etc/profile.d/conda.sh"
     conda activate testenv
     pip list
 elif [[ "$PACKAGER" == "ubuntu" ]]; then

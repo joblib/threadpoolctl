23fa7e1258928ba6df567bf462c81710a792df62
Itamar Turner-Trauring <itamar@itamarst.org>
2026-09-07 13:01:18 -0400

    Correct paths.

--- conftest.py
+++ conftest.py
@@ -1,5 +1,5 @@
 collect_ignore = [
-    "tests/empirical_scope_observation",
-    "tests/_openmp_test_helper",
-    "tests/_limit_blas",
+    "tests/empirical_scope_observation.py",
+    "tests/_openmp_test_helper.py",
+    "tests/_limit_blas.py",
 ]

1ac9590f721b0d0764bb8968d6908496fc20fda9
Itamar Turner-Trauring <itamar@itamarst.org>
2026-09-08 05:01:15 -0400

    Document using limiting APIs for Python thread pools (#234)

--- conftest.py
+++ conftest.py
@@ -1,1 +1,5 @@
-collect_ignore = ["tests/_openmp_test_helper", "tests/_limit_blas"]
+collect_ignore = [
+    "tests/empirical_scope_observation.py",
+    "tests/_openmp_test_helper.py",
+    "tests/_limit_blas.py",
+]
